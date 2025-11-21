9fb3a6f88d2f07681f8b9fc304f349d7d7a9fa87
Pham Manh Hung <hungpmhe176671@fpt.edu.vn>
2025-11-21 10:46:29 +0700

    update be

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const authRoutes = require('./authRoutes'); 
+
+const app = express();
+const PORT = 5000; 
+
+app.use(cors()); 
+app.use(bodyParser.json());
+app.use('/api', authRoutes); 
+
+
+
+app.listen(PORT, () => {
+    console.log(`\n========================================================`);
+    console.log(`    BACKEND API ĐANG CHẠY`);
+    console.log(`    Địa chỉ: http://localhost:${PORT}`);
+    console.log(`   - Auth: POST http://localhost:${PORT}/api/register`);
+    console.log(`========================================================\n`);
+});
