8a8ae6d019eb762b1d56ab792fb45e40a30d1fb4
Le Hoang Long <lelong3304@gmail.com>
2025-11-25 12:57:01 +0700

    Update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import { checkDbConnection } from './config/database';
+import passport from './config/passport';
 
 // Import Routes
 import authRoutes from './routes/AuthRoutes';
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware cơ bản
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 // Route mặc định
 app.get('/', (req, res) => {
